65d33624a3ce8157368bab9e1df546cc43497239
TianciHuang <tianci.huang@qq.com>
2017-03-06 23:55:16 +0800

    security配置

--- src/main/java/com/huangtianci/myframeprototype/mvc/config/SecurityConfig.java
+++ src/main/java/com/huangtianci/myframeprototype/mvc/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.huangtianci.myframeprototype.mvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,5 +15,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER").and()
                 .withUser("admin").password("password").roles("USER","ADMIN");
+    }
+
+    /**
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //每个macher按照他们的声明顺序执行
+                //我们指定任何用户都可以访问的多个URL模式。任何用户都可以访问URL以 "/resources/",开头的URL ,以及"/signup", "/about".
+                .antMatchers("/resources/**", "/signup", "/about").permitAll()
+                //以"/admin/" 开头的URL只能由拥有 "ROLEADMIN"角色的用户访问. 请注意我们使用HasRole方法，没有使用ROLE前缀。
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                //任何以/db/开头的URL需要用户同时具有"ROLEADMIN" 和 "ROLE_DBA". 和上面一样我们的hasRole方法也没有使用ROLE前缀。
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                //确保我们应用中的所有请求都需要用户被认证
+                .anyRequest().authenticated()
+            .and()
+            //允许用户进行基于表单的认证,指定登陆页的路径并必须允许所有用户访问我们的登录页
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            //允许用户使用HTTP基本验证进行认证
+            .httpBasic();
     }
 }
---
+++ src/main/java/com/huangtianci/myframeprototype/mvc/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.huangtianci.myframeprototype.mvc.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Tianci.Huang on 2017/3/6.
+ * 为应用程序的所有URL注册SpringSecurityFilterChain
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
