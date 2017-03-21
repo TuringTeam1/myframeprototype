9cba6a9241fc720d2c7f1fd8c02c943a20426617
TianciHuang <huang_tianci@sina.com>
2017-03-21 20:19:26 +0800

    angularjs

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,9 +1,25 @@
 var myModule = angular.module("HelloAngular" ,[]);
 
-myModule.controller("helloAngular",["$scope",
-    function HelloAngular($scope){
-        $scope.greeting = {
-            "text" : "Hello"
-        }
+myModule.controller("helloAngular",[
+    function HelloAngular(){
+        var self = this;
+        self.notes = [
+            {
+                id:1,
+                label: 'First Node',
+                done: false
+            },
+            {
+                id:2,
+                label: 'Second Note',
+                done: false
+            },
+            {
+                id:3,
+                label: 'Finished Third Note',
+                done: true
+            }
+        ];
+
     }
 ]);
